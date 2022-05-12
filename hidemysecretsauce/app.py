fbe5f596a4987629d1546885d86a39ea51f616ab
Emil Kovacev <emilkova@buffalo.edu>
2022-05-12 15:47:13 -0400

    working on integrating qr code display

--- hidemysecretsauce/app.py
+++ hidemysecretsauce/app.py
@@ -1,34 +1,28 @@
 import secrets
 from flask import Flask, request, make_response, \
     render_template, url_for, redirect
-from database import users, sauces
+from database import *
 import bcrypt
 import re
 from bson.objectid import ObjectId
-
-
+import qrcode
+import pyotp
 
 
 app = Flask(__name__)
 SECURE = False
 
 
-
 @app.route('/')
 def home():
     token = request.cookies.get('token', None)
     if token:
-        style = url_for('static', filename='css/style.css')
-        js = url_for('static', filename='js/sauce.js')
-        user = users.find_one({'token': token})
-        sauce_list = sauces.find({'username': user['username']})
-        if sauce_list is None:
-            sauce_list = []
+        user = get_user_by_token(token)
+        sauce_list = get_sauce_by_username(user['username']) 
         return render_template(
-            'sauces.html', 
+            'sauces.html',
+            secure=SECURE,
             username=user['username'], 
-            style=style, 
-            js=js, 
             sauces=sauce_list
         )
     else:
@@ -38,33 +32,30 @@ def home():
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        style = url_for('static', filename='css/style.css')
-        return render_template(
-            'form.html', 
-            style=style, 
-            form_title='Login',
-            action='/login'
-        )
+        return render_template('login.html')
 
     else:
         # parse data from request
         username, password = request.form['username'], request.form['password']
-        return login_auth(username, password) 
+
+        # if user doesn't exist or password incorrect
+        if not user_exists(username):
+            return 'login error', 403
 
 
-# authenticate user given username and password
-def login_auth(username, password):
-    user = users.find_one({'username': username})
+        token = secrets.token_bytes(20)
+        add_refresh_token(username, token)
+        response = redirect('/')
+        response.set_cookie('token', str(token))
+        return response
 
-    # if user doesn't exist or password incorrect
-    if user == None or not bcrypt.checkpw(password.encode('utf8'), user['password']):
-        return 'login error', 403
 
-    token = secrets.token_bytes(20)
-    users.update_one({'username': username}, { '$push': { 'token': str(token) } })
-    response = redirect('/')
-    response.set_cookie('token', str(token))
-    return response
+@app.route('/check_2fa', methods=['GET', 'POST'])
+def check_2fa():
+    if request.method == 'GET':
+        return render_template('/check_2fa.html')
+    else:
+        pass
 
 
 @app.route('/logout', methods=['GET', 'POST'])
@@ -82,13 +73,7 @@ def logout():
 @app.route('/signup', methods = ['GET', 'POST'])
 def signup():
     if request.method == 'GET':
-        style = url_for('static', filename='css/style.css')
-        return render_template(
-            'form.html', 
-            style=style, 
-            form_title='Sign In',
-            action='/signup'
-        )
+        return render_template('signup.html')
 
     else:
         # parse data from request
@@ -97,14 +82,33 @@ def signup():
             return 'Invalid Password'
 
         # check if user already exists
-        if users.count_documents({'username': username}) != 0:
+        if user_exists(username):
             return 'user exists'
 
         # insert new user
-        hashed = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
-        users.insert_one({'username': username, 'password': hashed })
-        return login_auth(username, password)
+        add_user(username, password) 
 
+        return redirect('/login')
+
+
+@app.route('/setup_2fa', methods=['GET', 'POST'])
+def setup_2fa():
+    if request.method == 'GET' and 'token' in request.cookies:
+        secret_key = pyotp.random_base32()
+        user = get_user_by_token(request.cookies['token'])
+        username = user['username']
+
+        qr_code_data = pyotp.totp.TOTP(secret_key).provisioning_uri(
+            name=username, 
+            issuer_name='Hide My Secret Sauce'
+        )
+
+        qr_code = qrcode.make(qr_code_data)
+        return render_template('setup_2fa.html', data=qr_code)
+
+    else:
+        # code, password = request.form['code'], request.form['password']
+        return 'POST request to /setup_2fa'
 
 @app.route('/add_sauce', methods=['POST'])
 def add_sauce():
@@ -122,8 +126,8 @@ def add_sauce():
         ingredients = [x.title() for x in ingredients]
 
         # save sauce
-        user = users.find_one({'token': token})
-        sauces.insert_one({'username': user['username'], 'name': sauce, 'ingredients': ingredients})
+        user = get_user_by_token(token)
+        add_sauce(user['username'], sauce, ingredients)
         return redirect('/')
 
     else:
@@ -134,12 +138,10 @@ def delete_sauce():
     token = request.cookies.get('token', None)
     if token:
         _id = request.json['_id']
-        sauce = sauces.find_one({'_id': ObjectId(_id)})
-        user = users.find_one({'token': token})
-        if sauce['username'] != user['username']:
+        try:
+            delete_sauce(token, _id)
+        except ValueError:
             return 'Invalid Request', 403
-        
-        sauces.delete_one({'_id': ObjectId(_id)})
         return redirect('/')
 
     else:
--- hidemysecretsauce/database.py
+++ hidemysecretsauce/database.py
@@ -4,3 +4,51 @@ client = pymongo.MongoClient()
 db = client["sauce"]
 users = db["users"]
 sauces = db["sauces"]
+
+
+def get_user_by_username(username):
+    return users.find_one({'username': username})
+
+def get_user_by_token(token):
+    return users.find_one({'token': token})
+
+def validate_password(username, password):
+    user = get_user(username)
+    # if user doesn't exist or password incorrect
+    if user is None or not bcrypt.checkpw(password.encode('utf8'), user['password']):
+        return False
+
+    return True
+
+def user_exists(username):
+    return users.count_documents({'username': username}) != 0 
+
+def add_user(username, password):
+    hashed = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+    users.insert_one({'username': username, 'password': hashed })
+
+def add_refresh_token(username, token):
+    users.update_one({'username': username}, { '$push': { 'token': str(token) } })
+    return token
+
+
+
+
+
+def add_sauce(username, sauce, ingredients):
+    sauces.insert_one({'username': user['username'], 'name': sauce, 'ingredients': ingredients})
+
+def delete_sauce(token, sauce_id):
+    sauce = sauces.find_one({'_id': ObjectId(_id)})
+    user = users.find_one({'token': token})
+    if sauce['username'] != user['username']:
+        raise ValueError
+
+    sauces.delete_one({'_id': ObjectId(_id)})
+
+def get_sauce_by_username(username):
+    sauce_list = sauces.find({'username': user['username']})
+    if sauce_list is None:
+        sauce_list = []
+    return sauce_list
+
